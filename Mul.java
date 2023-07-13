393dc42258e474353159b2b96ad263b01c4dcf79
Ezequiel Arias <bayron5757@gmail.com>
2023-07-13 17:35:13 -0600

    subiendo mutiplicacion

---
+++ Mul.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=5;
+      int y=5;
+      int resta=x*y;
+
+      System.out.println("La multiplicación de 5 - 5 es igual a  " + z);
+    }
+}
