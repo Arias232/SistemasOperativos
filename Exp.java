1af41a4f585ee16d974efc3b1f9a9dd4066aedcb
Ezequiel Arias <bayron5757@gmail.com>
2023-07-13 17:34:37 -0600

    subiendo exponente

---
+++ Exp.java
@@ -0,0 +1,8 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int y=5;
+      int resta=y*y;
+
+      System.out.println("El exponente al cuadrado de 5 es igual a " + z);
+    }
+}
