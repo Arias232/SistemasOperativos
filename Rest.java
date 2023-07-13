697a37d044830468ea68b69725a6dedfbb58a7ff
Ezequiel Arias <bayron5757@gmail.com>
2023-07-13 17:35:43 -0600

    subiendo resta

---
+++ Rest.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String args[]) {
+      int x=5;
+      int y=5;
+      int resta=x+y;
+
+      System.out.println("La resta de 5 - 5 es igual a  " + z);
+    }
+}
